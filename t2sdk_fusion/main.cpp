68baa3f65b3ba0b4bbfa5e2647e9e018011c6770
tudorex <359721272@qq.com>
2017-06-28 23:51:46 +0800

    json11 example fail on vs2013

--- t2sdk_fusion/main.cpp
+++ t2sdk_fusion/main.cpp
@@ -1,15 +1,34 @@
 #include "stdafx.h"
-#include "t2sdk_platform.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include "json11.hpp"
 
+using namespace json11;
+using namespace std;
+
+void LoadJson(string& out, const char* path)
+{
+	ifstream ifs(path);
+	assert(ifs.is_open());
+
+	string linetmp;
+	stringstream ss;
+	ss.clear();
+	ss.str("");
+	while (std::getline(ifs, linetmp))
+	{
+		ss << linetmp;
+	}
+	std::swap(out, ss.str());
+}
 int main()
 {
-	PersonMap_t person_map;
-	CarMap_t car_map;
-	RoomMap_t room_map;
+	string buf;
+	LoadJson(buf, "o32.json");
 
-	fusion::joint_view<
-		PersonMap_t,
-		CarMap_t,
-		RoomMap_t
-	> view(person_map, car_map, room_map);
+	string err;
+	auto json = Json::parse(buf, err);
 }
