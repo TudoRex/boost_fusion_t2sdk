1556a6b787335fb45ad7be1272009cab61dc2b32
tudorex <359721272@qq.com>
2017-07-02 23:22:44 +0800

    编译通过

--- t2sdk_fusion/main.cpp
+++ t2sdk_fusion/main.cpp
@@ -1,5 +1,6 @@
 #include <boost/lexical_cast.hpp>
 #include <string>
+#include <iostream>
 
 struct stringize_functor {
 private:
@@ -39,11 +40,32 @@ BOOST_FUSION_DEFINE_ASSOC_STRUCT(
 	(int		, age, keys::Age)
 )
 
+struct Visitor
+{
+	template<typename T>
+	void operator() (const T& t) const
+	{
+		std::cout << typeid(T).name() << std::endl;
+	}
+};
 
 #include <cassert>
 #include <boost/fusion/adapted/boost_tuple.hpp>
 #include <boost/fusion/adapted/std_pair.hpp>
 
+#include <boost/fusion/container/vector/vector.hpp>
+#include <boost/fusion/adapted/mpl.hpp>
+#include <boost/fusion/container/generation/make_vector.hpp>
+#include <boost/fusion/container/generation/make_list.hpp>
+#include <boost/fusion/container/map/convert.hpp>
+#include <boost/fusion/container/vector/convert.hpp>
+#include <boost/fusion/algorithm/transformation/push_back.hpp>
+#include <boost/fusion/sequence/comparison/equal_to.hpp>
+#include <boost/fusion/sequence/intrinsic/at_key.hpp>
+#include <boost/fusion/sequence/io/out.hpp>
+#include <boost/fusion/support/pair.hpp>
+
+namespace fusion = boost::fusion;
 int main() {
 	boost::tuple<char, int, char, int> decim('-', 10, 'e', 5);
 	assert(stringize(decim) == "-10e5");
@@ -51,5 +73,8 @@ int main() {
 	std::pair<short, std::string> value_and_type(270, "Kelvin");
 	assert(stringize(value_and_type) == "270Kelvin");
 
+	demo::myst me("amao", 18);
+
+	fusion::for_each(fusion::as_map(me), Visitor());
 
 }
